0d5326982f58a682a7c531a320e24e484a074ab3
ManishMaster <manish.rsauto@gmail.com>
2024-02-08 13:57:08 +0530

    modification: machinedata component new css update

--- app/components/DashboardData.jsx
+++ app/components/DashboardData.jsx
@@ -1,96 +1,11 @@
 "use client";
-// import React from "react";
-// import { useEffect, useState } from "react";
-// import MachineData from "./MachineData";
-// import Loader from "./Loader";
-// import Image from "next/image";
-// import internetError from "../../public/internetConnectivityError.svg";
-// const DashboardData = () => {
-//   const [machineData, setMachineData] = useState([]);
-//   const [loading, setLoading] = useState(true);
-//   const [errorMessage, setErrorMessage] = useState(null);
-//   const url =
-//     process.env.NODE_ENV !== "production"
-//       ? `${process.env.NEXT_PUBLIC_LOCAL_HOST}api/dashboard`
-//       : `${process.env.NEXT_PUBLIC_DOMAIN_NAME}api/dashboard`;
-
-//   const fetchData = async () => {
-//     try {
-//       console.log("fetch")
-//       // setLoading(true);
-//       // Fetch data from your API
-//       const response = await fetch(url, { cache: "no-store" });
-//       const responseData = await response.json();
-//       if (responseData.message) {
-//         setErrorMessage(responseData.message);
-//         setMachineData([]);
-//       } else {
-//         setMachineData(responseData);
-//         setErrorMessage(null);
-//       }
-//       setLoading(false);
-//     } catch (error) {
-//       console.error("Error fetching data:", error);
-//     }
-//   };
-
-//   useEffect(() => {
-//     fetchData();
-//   }, []);
-
-//   useEffect(() => {
-//     let interval = setInterval(async () => {
-//       fetchData();
-//     }, 3000);
-//     return () => {
-//       clearInterval(interval);
-//     };
-//   }, []);
-//   return (
-//     <div className="min-h-screen rounded overflow-hidden px-2 grid lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 gap-4 py-2">
-//       {loading ? (
-//         <>
-//           <Loader />
-//         </>
-//       ) : (
-//         <>
-//           {errorMessage ? (
-//             <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-//               <div className="py-2 text-center font-bold text-lg text-white">
-//                 <div className="flex flex-col items-center">
-//                   <Image
-//                     src={internetError}
-//                     alt="SVG Image"
-//                     width={100}
-//                     height={100}
-//                   />
-//                   <p className="text-center font-semibold text-white">
-//                     {errorMessage}
-//                   </p>
-//                 </div>
-//               </div>
-//             </div>
-//           ) : (
-//             <>
-//               {machineData?.slice(0, 5).map((data, i) => (
-//                 <div key={i} className="flex justify-center items-center">
-//                   <div className="w-full max-w-md">
-//                     <MachineData data={data} key={i} />
-//                   </div>
-//                 </div>
-//               ))}
-//             </>
-//           )}
-//         </>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default DashboardData;
-
 import React, { useState, useEffect } from "react";
 import MachineData from "./MachineData";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import { Navigation,Pagination } from "swiper/modules";
 import Loader from "./Loader";
 import Image from "next/image";
 import internetError from "../../public/internetConnectivityError.svg";
@@ -145,11 +60,34 @@ const DashboardData = () => {
       clearInterval(interval);
     };
   }, []);
-
+  // min-h-screen rounded overflow-hidden px-2 grid lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 gap-4 py-2
   return (
-    <div className="min-h-screen rounded overflow-hidden px-2 grid lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 gap-4 py-2">
+    <div className="content-container">
+    <Swiper
+      spaceBetween={20}
+      navigation={true}
+      modules={[Navigation,Pagination]}
+      breakpoints={{
+        520: {
+          // when window width is >= 520px
+          slidesPerView: 2,
+        },
+        640: {
+          slidesPerView: 3,
+        },
+        768: {
+          slidesPerView: 4,
+        },
+        1024: {
+          slidesPerView: 4,
+        },
+      }}
+      className="mySwiper"
+    >
       {loading ? (
-        <Loader />
+        <div className="min-h-[50vh]">
+          <Loader />
+        </div>
       ) : errorMessage ? (
         <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
           <div className="py-2 text-center font-bold text-lg text-white">
@@ -160,15 +98,14 @@ const DashboardData = () => {
           </div>
         </div>
       ) : (
-        Array.isArray(machineData) && machineData?.slice(0, 5).map((data, i) => (
-          <div key={i} className="flex justify-center items-center">
-            <div className="w-full max-w-md">
-              <MachineData data={data} key={i} />
-            </div>
-          </div>
+        Array.isArray(machineData) && machineData?.slice(0, 10).map((data, i) => (
+          <SwiperSlide key={i}>
+             <MachineData data={data} key={i} />
+          </SwiperSlide>
         ))
       )}
-    </div>
+    </Swiper>
+  </div>
   );
 };
 
--- app/components/MachineData.jsx
+++ app/components/MachineData.jsx
@@ -3,17 +3,11 @@ import React from "react";
 const MachineData = ({ data }) => {
   return (
     <div>
-      <div className="bg-gray-200 h-[372px] flex flex-col space-y-1 border-4 border-white rounded ">
+      {/* <div className="bg-gray-200 h-[372px] flex flex-col space-y-1 border-4 border-white rounded ">
         <h5 className="font-semibold text-center text-white bg-[#0891B2] rounded-t">
           Machine Number :{" "}
           <span className=" text-lg text-white"> {data.Machine_no} </span>
         </h5>
-        {/* <h5 className="font-semibold text-center bg-slate-800 text-white">
-          Machine Name
-        </h5> */}
-        {/* <h5 className="font-semibold text-center bg-blue-600 text-white">
-          {data.machineName}
-        </h5> */}
         <h5 className="realtimelabelwarpper">
           Speed (Pcs/Hr)
         </h5>
@@ -50,10 +44,46 @@ const MachineData = ({ data }) => {
         <h5 className={`realtimedatawarpper ${data.Status==='RUN'? 'text-green-500':'text-red-500'}  rounded-b`}>
           {data.Status}
         </h5>
+      </div> */}
+      <div className="bg-[#162637] h-[372px] flex flex-col space-y-1 border-4 border-white rounded px-1 ">
+        <div className="flex flex-col space-y-1">
+          <label htmlFor="status" className="text-center text-white font-bold">
+            {`Machine ${data.Machine_no}`}
+          </label>
+          <h5
+            className={`${
+              data.Status === "RUN" ? "bg-[#19DAAD]" : "bg-[#EE574D]"
+            }  rounded text-white text-center font-bold h-10 flex-card text-lg`}
+            id="status"
+          >
+            {data.Status}
+          </h5>
+        </div>
+            <div className="space-y-1">
+                <div className="space-x-1 flex">
+                  <label htmlFor="speed" className="bg-[#25384A] p-1 rounded text-white text-base inline-block w-48" > Speed (Pcs/Hr)</label>
+                  <span className="p-1 flex-1 text-right border-b-2 border-[#2F4758] text-[#19DAAD]">{data.Speed}</span>
+                </div>
+                <div className="space-x-1 flex">
+                  <label htmlFor="speed" className="bg-[#25384A] p-1 rounded text-white text-base inline-block w-48" > Production (Piece)</label>
+                  <span className="p-1 flex-1 text-right border-b-2 border-[#2F4758] text-[#19DAAD]">{data.Production}</span>
+                </div>
+                <div className="space-x-1 flex">
+                  <label htmlFor="speed" className="bg-[#25384A] p-1 rounded text-white text-base inline-block w-48" > Runtime</label>
+                  <span className="p-1 flex-1 text-right border-b-2 border-[#2F4758] text-[#19DAAD]">{data.Runtime}</span>
+                </div>
+                <div className="space-x-1 flex">
+                  <label htmlFor="speed" className="bg-[#25384A] p-1 rounded text-white text-base inline-block w-48" > Efficiency (%)</label>
+                  <span className="p-1 flex-1 text-right border-b-2 border-[#2F4758] text-[#19DAAD]">{data.Efficiency}</span>
+                </div>
+                <div className="space-x-1 flex">
+                  <label htmlFor="speed" className="bg-[#25384A] p-1 rounded text-white text-base inline-block w-48" > Energy (Kwhr)</label>
+                  <span className="p-1 flex-1 text-right border-b-2 border-[#2F4758] text-[#19DAAD]">{data.Kwhr}</span>
+                </div>
+            </div>
       </div>
     </div>
   );
 };
 
 export default MachineData;
-
